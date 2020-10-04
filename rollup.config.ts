7e7960d71b42e06f62353dcbaacc4ab16e698f43
yassipad <yasser.lahbibi@apenhet.com>
2020-10-04 20:32:16 +0200

    chore(initial): initial commit

---
+++ rollup.config.ts
@@ -0,0 +1,58 @@
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+import sourceMaps from 'rollup-plugin-sourcemaps'
+import camelCase from 'lodash.camelcase'
+import typescript from 'rollup-plugin-typescript2'
+import json from 'rollup-plugin-json'
+import { terser } from 'rollup-plugin-terser'
+
+const pkg = require('./package.json')
+
+const libraryName = pkg.name.split('/')[1]
+
+export default {
+  input: `src/index.ts`,
+  output: [
+    {
+      file: pkg.main,
+      name: camelCase(libraryName),
+      format: 'umd',
+      sourcemap: true,
+      globals: {
+        fabric: 'fabric',
+      }
+    },
+    {
+      file: pkg.module,
+      format: 'es',
+      sourcemap: true,
+      plugins: [terser()]
+    },
+    {
+      file: pkg.module.replace(/\.js$/, '.min.js'),
+      format: 'es',
+      sourcemap: true,
+      plugins: [terser()]
+    }
+  ],
+  // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
+  external: ['fabric'],
+  watch: {
+    include: 'src/**',
+  },
+  plugins: [
+    // Allow json resolution
+    json(),
+    // Compile TypeScript files
+    typescript({ useTsconfigDeclarationDir: true }),
+    // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
+    commonjs(),
+    // Allow node_modules resolution, so you can use 'external' to control
+    // which external modules to include in the bundle
+    // https://github.com/rollup/rollup-plugin-node-resolve#usage
+    resolve(),
+
+    // Resolve source maps to the original source
+    sourceMaps(),
+  ],
+}
